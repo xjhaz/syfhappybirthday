7dbdca06de5b50d612ee5666fd7529c162e0d1e5
xjhaz <73150297+xjhaz@users.noreply.github.com>
2021-09-11 13:03:31 +0800

    11

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="石艳菲" &&  pwd=="生日快乐"){
+    if(userName=="石艳菲" &&  pwd=="happybirthday"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
